ab933267b3e1f59daee934ad3f05148298e14afe
Hamza Bashir <hamzabashir218@gmail.com>
2023-07-06 18:20:36 +0500

    Homepage done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import { fontFamily as _fontFamily } from "tailwindcss/defaultTheme";
 
+export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      Roboto: ['"Roboto Condensed"', ..._fontFamily.sans],
+    },
+  },
+};
+export const plugins = [];
